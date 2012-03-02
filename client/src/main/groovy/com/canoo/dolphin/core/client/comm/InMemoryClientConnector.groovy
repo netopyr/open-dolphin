52c707e63ab21b7e8c909d64b1144d488c68e99e
Dierk Koenig <dierk.koenig@canoo.com>
2012-03-02 08:21:11 +0100

    make the in-memory connector more fault tolerant

--- client/src/main/groovy/com/canoo/dolphin/core/client/comm/InMemoryClientConnector.groovy
+++ client/src/main/groovy/com/canoo/dolphin/core/client/comm/InMemoryClientConnector.groovy
@@ -1,14 +1,19 @@
 package com.canoo.dolphin.core.client.comm
 
 import com.canoo.dolphin.core.comm.Command
+import groovy.util.logging.Log
 
-@Singleton
+@Singleton @Log
 class InMemoryClientConnector extends ClientConnector {
 
     def receiver // must be injected since the class is only available in a "combined" context
 
     @Override
     List<Command> transmit(Command command) {
-        List<Command> response = receiver.receive(command) // there is no need for encoding since we are in-memory
+        if (!receiver) {
+            log.warning "no receiver wired for in-memory connector"
+            return Collections.EMPTY_LIST
+        }
+        receiver.receive(command) // there is no need for encoding since we are in-memory
     }
 }
