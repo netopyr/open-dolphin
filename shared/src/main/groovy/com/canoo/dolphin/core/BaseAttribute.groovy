1479e5151c7b8822c4e908fc8996098bd1ac9b59
Dierk Koenig <dierk.koenig@canoo.com>
2012-03-22 11:41:06 +0100

    housekeeping

--- shared/src/main/groovy/com/canoo/dolphin/core/BaseAttribute.groovy
+++ shared/src/main/groovy/com/canoo/dolphin/core/BaseAttribute.groovy
@@ -3,9 +3,8 @@ package com.canoo.dolphin.core
 import groovy.beans.Bindable
 
 /**
- An BaseAttribute represents the (transient) value of a property of a backing bean (server) or its server counterpart (client).
  The value may be null as long as the BaseAttribute is used as a "placeholder".
- * */
+*/
 
 class BaseAttribute {
 
@@ -13,7 +12,7 @@ class BaseAttribute {
     final String propertyName
 
     /** may be null **/
-    @Bindable Object value
+    @Bindable Object value // setter contains really-changes-value check
 
     BaseAttribute(String propertyName) {
         assert propertyName
