4c734cf0e24a27922760693ed5b35a284819c8e6
Dieter Holz <dieter.holz@canoo.com>
2012-07-18 21:54:31 +0200

    signature of PresentationModelListChangedListener.removed improved
    BindListView doesn't use valueProperty any more (and ClientAttributeWrapper instead)

--- subprojects/client/src/main/groovy/com/canoo/dolphin/core/client/PresentationModelListChangedListener.groovy
+++ subprojects/client/src/main/groovy/com/canoo/dolphin/core/client/PresentationModelListChangedListener.groovy
@@ -5,5 +5,5 @@ package com.canoo.dolphin.core.client
  */
 interface PresentationModelListChangedListener {
 	void added(ClientPresentationModel pm)
-	void removed(ClientPresentationModel)
+	void removed(ClientPresentationModel pm)
 }
