a8da48106d19efe5ab20803e281876cd3b4e36ef
Dierk Koenig <dierk.koenig@canoo.com>
2012-07-20 15:39:18 +0200

    move all JavaFX dependencies into client-javafx.

--- subprojects/client-javafx/src/main/groovy/com/canoo/dolphin/core/client/comm/JavaFXUiThreadHandler.java
+++ subprojects/client-javafx/src/main/groovy/com/canoo/dolphin/core/client/comm/JavaFXUiThreadHandler.java
@@ -2,8 +2,6 @@ package com.canoo.dolphin.core.client.comm;
 
 import javafx.application.Platform;
 
-// todo dk: remove dependency to JavaFX - move this handler to an extra module
-
 public class JavaFXUiThreadHandler implements UiThreadHandler {
     @Override
     public void executeInsideUiThread(Runnable runnable) {
---
+++ subprojects/combined/src/main/groovy/com/canoo/dolphin/core/comm/DefaultInMemoryConfig.groovy
@@ -0,0 +1,42 @@
+package com.canoo.dolphin.core.comm
+
+import com.canoo.dolphin.LogConfig
+import com.canoo.dolphin.core.ModelStore
+import com.canoo.dolphin.core.client.ClientModelStore
+import com.canoo.dolphin.core.client.Dolphin
+import com.canoo.dolphin.core.client.comm.ClientConnector
+import com.canoo.dolphin.core.client.comm.InMemoryClientConnector
+import com.canoo.dolphin.core.server.action.CreatePresentationModelAction
+import com.canoo.dolphin.core.server.action.StoreAttributeAction
+import com.canoo.dolphin.core.server.action.StoreValueChangeAction
+import com.canoo.dolphin.core.server.action.SwitchPmAction
+import com.canoo.dolphin.core.server.comm.Receiver
+
+class DefaultInMemoryConfig {
+
+    Receiver receiver = new Receiver()
+    ModelStore modelStore = new ModelStore()
+
+    DefaultInMemoryConfig() {
+        LogConfig.logCommunication()
+        connector.sleepMillis = 100
+        connector.receiver = receiver
+        Dolphin.setClientConnector(connector)
+        Dolphin.setClientModelStore(new ClientModelStore())
+    }
+
+    ClientConnector getConnector() { InMemoryClientConnector.instance }
+
+    void registerDefaultActions() {
+        [
+                new StoreValueChangeAction(modelStore),
+                new StoreAttributeAction(modelStore),
+                new CreatePresentationModelAction(modelStore),
+                new SwitchPmAction(modelStore),
+        ].each { register it }
+    }
+
+    void register(action) {
+        action.registerIn receiver.registry
+    }
+}
