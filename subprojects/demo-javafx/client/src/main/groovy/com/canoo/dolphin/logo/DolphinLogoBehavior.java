0adce5628ff061d80585f584a6c5f79e106498bd
Dierk Koenig <dierk.koenig@canoo.com>
2012-07-20 18:28:51 +0200

    putting the logo in

---
+++ subprojects/demo-javafx/client/src/main/groovy/com/canoo/dolphin/logo/DolphinLogoBehavior.java
@@ -0,0 +1,12 @@
+package com.canoo.dolphin.logo;
+
+import com.sun.javafx.scene.control.behavior.BehaviorBase;
+
+
+public class DolphinLogoBehavior extends BehaviorBase<DolphinLogo> {
+
+    public DolphinLogoBehavior(final DolphinLogo CONTROL) {
+        super(CONTROL);
+    }
+}
+
