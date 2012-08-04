a27b249100f756be082f17618ca1d5dba4ef5589
Dierk Koenig <dierk.koenig@canoo.com>
2012-08-05 00:54:10 +0200

    groovyfy the dolphin logo. use startLogo and click on it ;-)

--- subprojects/client-javafx/src/main/groovy/com/canoo/dolphin/logo/startLogo.groovy
+++ subprojects/client-javafx/src/main/groovy/com/canoo/dolphin/logo/startLogo.groovy
@@ -2,19 +2,29 @@
 package com.canoo.dolphin.logo
 
 import groovyx.javafx.GroovyFX
+import javafx.scene.shape.Path
 
 GroovyFX.start { app ->
 
-    DolphinLogo dolphinLogo = new DolphinLogo()
-    dolphinLogo.setPrefSize(401, 257)
+    List<Path> strokes = new DolphinLogoPaths(401, 257).paths()
 
-    stage title: "Fun with Dolphin", {
+    stage title: "Tickle the Dolphin!", {
         scene width:441, height: 297, {
-            stackPane id:'stack'
+            stackPane id:'stack', {
+                rectangle x: 0, y: 0, width: 441, height: 297, opacity: 0d
+                allAnimations = parallelTransition()
+                group id: 'logo',{
+                    for(stroke in strokes){
+                        path(stroke) {
+                            allAnimations.children <<
+                                rotateTransition(2.s, to: -360, onFinished: { it.source.targetNode.rotate = 0d} )
+                        }
+                    }
+                }
+                onMouseClicked { allAnimations.playFromStart() }
+            }
         }
     }
-
-    stack.children.add(dolphinLogo)
 
     primaryStage.show()
 }
