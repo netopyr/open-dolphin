9f92b6c3c9abd42b1bed3aa0ede6845a10173592
Dierk Koenig <dierk.koenig@canoo.com>
2012-08-05 05:33:16 +0200

    add pulse to logo

--- subprojects/client-javafx/src/main/groovy/com/canoo/dolphin/logo/startLogo.groovy
+++ subprojects/client-javafx/src/main/groovy/com/canoo/dolphin/logo/startLogo.groovy
@@ -4,6 +4,7 @@ package com.canoo.dolphin.logo
 import groovyx.javafx.GroovyFX
 
 GroovyFX.start { app ->
+    boolean first=true
 
     def effect = dropShadow(offsetY: 2, offsetX: 2, radius: 3, color:grey, input: lighting{distant(azimuth: -135.0)})
     logo = new DolphinLogo(width:401, height: 257, shuffle: true, effect: effect)
@@ -11,6 +12,18 @@ GroovyFX.start { app ->
     stage title: "Tickle the Dolphin!", {
         scene width:441, height: 297, {
             logo.addTo delegate
+            circle id:"pulse", fill:transparent, stroke:rgb(207, 0, 58), strokeWidth: 3, opacity:0, translateX:-100, translateY: -20, effect:boxBlur(), {
+                anim = timeline cycleCount: 3, {
+                    onFinished { pulse.radius = 10 }
+                    at (1.s) { change(pulse, "radius") to 200 tween ease_in }
+                    at (1.s) { change(pulse, "opacity") to 0  tween linear  }
+                }
+            }
+            onMouseClicked {
+                if (first) {first=false; return}
+                pulse.opacity = 1
+                anim.play()
+            }
         }
     }
     primaryStage.show()
