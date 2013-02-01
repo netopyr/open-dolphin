d84ee789d68c46b048e990d9846e000d2549e2bd
Dierk Koenig <dierk.koenig@canoo.com>
2013-02-01 13:08:44 +0100

    server side of the many-events demo

---
+++ dolphin-grails/grails-app/controllers/demo/ManyEventsController.groovy
@@ -0,0 +1,21 @@
+package demo
+
+import com.canoo.dolphin.core.server.EventBus
+
+class ManyEventsController {
+
+    EventBus manyEventsBus
+
+    def index() {
+        def soMany = params.times?.toInteger() ?: 10
+        int color = 0
+        soMany.times {
+            for (speed in 1..100) {
+                manyEventsBus.publish(null, [speed, color])
+                sleep (params.sleep.toInteger() ?: 20)
+            }
+            color++
+        }
+        render text:"done"
+    }
+}
