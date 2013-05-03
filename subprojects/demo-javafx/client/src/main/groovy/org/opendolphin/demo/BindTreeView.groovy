0b5ff5713fcdaa9e9e5dc003f715032cb4a32737
Dierk Koenig <dierk.koenig@canoo.com>
2013-05-03 23:27:56 +0200

    demo: a simple demo for how to model a tree for presentation from a tree-like domain

---
+++ subprojects/demo-javafx/client/src/main/groovy/org/opendolphin/demo/BindTreeView.groovy
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2012-2013 Canoo Engineering AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opendolphin.demo
+
+import javafx.scene.control.TreeItem
+import javafx.scene.control.TreeView
+import org.opendolphin.core.client.ClientDolphin
+import org.opendolphin.core.client.ClientPresentationModel
+
+import static groovyx.javafx.GroovyFX.start
+
+/**
+ * The simplemost demo how to have a tree modeled as presentation models
+ * and binding it to a treeview.
+ */
+
+class BindTreeView {
+
+    static show(ClientDolphin dolphin) {
+
+        start { app ->
+            TreeView tree
+            stage {
+                scene width: 300, height: 300, {
+                    tree = treeView()
+                }
+            }
+
+            dolphin.send "PULL_TREE", { List<ClientPresentationModel> pms ->
+                def root = pms.find { it.parent.value == null }
+                tree.root = filledTreeItem(pms, root)
+                tree.root.expanded = true
+                tree.showRoot = true
+            }
+
+            primaryStage.show()
+        }
+    }
+
+    static TreeItem filledTreeItem(allNodeModels, current) {
+        def result = new TreeItem<String>(current.id)
+        allNodeModels.findAll { it.parent.value == current.id }.each {
+            result.children << filledTreeItem(allNodeModels, it)
+        }
+        return result
+    }
+}
---
+++ subprojects/demo-javafx/server/src/main/groovy/org/opendolphin/demo/PullTreeActionHandler.groovy
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2012-2013 Canoo Engineering AG.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.opendolphin.demo
+
+import org.opendolphin.core.comm.Command
+import org.opendolphin.core.comm.NamedCommand
+import org.opendolphin.core.server.DTO
+import org.opendolphin.core.server.ServerDolphin
+import org.opendolphin.core.server.Slot
+import org.opendolphin.core.server.comm.NamedCommandHandler
+
+class PullTreeActionHandler implements NamedCommandHandler {
+
+    void handleCommand(NamedCommand command, List<Command> response) {
+        // a stand-in for some arbitrary kind of tree in the application domain
+        def domainTreeRoot = new NodeBuilder().earth {
+            europe {
+                switzerland {
+                    basel()
+                    zurich()
+                }
+                germany()
+            }
+            america()
+            australia()
+        }
+
+        makePM(domainTreeRoot, response)
+    }
+
+    private void makePM(node, List<Command> response) {
+        DTO model = new DTO(new Slot("parent", node.parent()?.name()))
+        ServerDolphin.presentationModel(response, node.name(), null, model)
+        node.children().each {
+            makePM it, response // recurse
+        }
+    }
+}
