d4ef12573c464acdd77214b70deed8179270660f
Dierk Koenig <dierk.koenig@canoo.com>
2013-07-28 14:22:03 +0200

    make the demo grails server use version 0.8

--- dolphin-grails/grails-app/conf/BuildConfig.groovy
+++ dolphin-grails/grails-app/conf/BuildConfig.groovy
@@ -37,7 +37,7 @@ grails.project.dependency.resolution = {
     dependencies {
         compile 'org.codehaus.gpars:gpars:1.0.0'
 
-        def dolphinVersion = '0.8-SNAPSHOT'
+        def dolphinVersion = '0.8'
 
         compile "org.open-dolphin:dolphin-shared:$dolphinVersion"
         compile "org.open-dolphin:dolphin-server:$dolphinVersion"
