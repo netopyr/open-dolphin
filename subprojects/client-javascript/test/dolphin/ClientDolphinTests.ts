a28d18d9df93f25cae9625fe268badc57adcce8b
kunsingh <kunal.singh@canoo.com>
2013-11-21 15:53:27 +0100

    DOL-98 : Fixed ClientDolphinTests- set HttpClientConnector to ClientDolphin.

--- subprojects/client-javascript/test/dolphin/ClientDolphinTests.ts
+++ subprojects/client-javascript/test/dolphin/ClientDolphinTests.ts
@@ -3,6 +3,7 @@ import ca     = require("../../js/dolphin/ClientAttribute")
 import cpm    = require("../../js/dolphin/ClientPresentationModel")
 import cd     = require("../../js/dolphin/ClientDolphin")
 import cms     = require("../../js/dolphin/ClientModelStore")
+import cc     = require("../../js/dolphin/HttpClientConnector")
 
 
 export module dolphin {
@@ -12,6 +13,7 @@ export module dolphin {
             var clientDolphin:cd.dolphin.ClientDolphin = new cd.dolphin.ClientDolphin();
             var clientModelStore:cms.dolphin.ClientModelStore = new cms.dolphin.ClientModelStore(clientDolphin);
             clientDolphin.setClientModelStore(clientModelStore);
+            clientDolphin.setClientConnector(new cc.dolphin.HttpClientConnector());
             var pm1:cpm.dolphin.ClientPresentationModel=clientDolphin.presentationModel("myId","myType");
             this.areIdentical(pm1.id,"myId");
             this.areIdentical(pm1.attributes.length,0);
