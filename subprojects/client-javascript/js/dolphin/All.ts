7136c2df7f855a24109708f492b94053b5ebae7a
Dierk Koenig <dierk.koenig@canoo.com>
2013-12-05 22:52:40 +0100

    Added the "All" facade to make using the ts code from js easier.

--- subprojects/client-javascript/js/dolphin/All.ts
+++ subprojects/client-javascript/js/dolphin/All.ts
@@ -4,12 +4,20 @@ import mst = require('../../js/dolphin/ClientModelStore');
 import cc  = require('../../js/dolphin/ClientConnector');
 import ntm = require('../../js/dolphin/NoTransmitter');
 
-// setting up the dolphin
+/**
+ * JS-friendly facade to avoid too many dependencies in plain JS code
+ */
 
+// setting up the dolphin
 export var dolphin = new dol.dolphin.ClientDolphin();
 var connector = new cc.dolphin.ClientConnector(new ntm.dolphin.NoTransmitter());
 dolphin.setClientConnector(connector);
 dolphin.setClientModelStore(new mst.dolphin.ClientModelStore(dolphin));
+
+// factory method for attributes
+export function attribute(propertyName, qualifier) {
+    return new cat.dolphin.ClientAttribute(propertyName, qualifier);
+}
 
 
 
