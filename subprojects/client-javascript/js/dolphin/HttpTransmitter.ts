b427ce296edd0f84cc9ac0567ab3056c6608941d
kunsingh <kunal.singh@canoo.com>
2013-12-05 19:56:49 +0100

    DOL-98 :Added Codec and URl to HttpTransmitter.

--- subprojects/client-javascript/js/dolphin/HttpTransmitter.ts
+++ subprojects/client-javascript/js/dolphin/HttpTransmitter.ts
@@ -1,13 +1,17 @@
 import cmd = require("../../js/dolphin/Command")
 import cc  = require("../../js/dolphin/ClientConnector")
+import cod = require("../../js/dolphin/Codec")
+
 export module dolphin {
 
     export class HttpTransmitter implements cc.dolphin.Transmitter {
 
         http:XMLHttpRequest;
+        codec:cod.dolphin.Codec
 
         constructor(public url:string) {
             this.http = new XMLHttpRequest();
+            this.codec = new cod.dolphin.Codec();
         }
 
         transmit(commands:cmd.dolphin.Command[], onDone:(result:cmd.dolphin.Command[]) => void):void {
@@ -22,7 +26,7 @@ export module dolphin {
             }
 
             this.http.open('POST', 'http://localhost:8080/dolphin-grails/dolphin/', true);
-            this.http.send();
+            this.http.send(this.codec.encode(commands));
 
         }
 
