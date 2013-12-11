4415deb543177ea60398a6606d831642b5b9974c
kunsingh <kunal.singh@canoo.com>
2013-12-11 21:48:36 +0100

    DOL-98 : Added test cases for ClientPresentationModel.

--- subprojects/client-javascript/test/dolphin/ClientPresentationModelTests.ts
+++ subprojects/client-javascript/test/dolphin/ClientPresentationModelTests.ts
@@ -107,5 +107,57 @@ export module dolphin {
             //PM should not be dirty
             this.areIdentical(pm.isDirty(),false);
         }
+
+        findAttributeById(){
+            var pm = new cpm.dolphin.ClientPresentationModel(undefined,undefined);
+            var ca1 = new ca.dolphin.ClientAttribute("prop1","qual1","value1","VALUE");
+
+            pm.addAttribute(ca1);
+            var result = pm.findAttributeById(ca1.id);
+            this.areIdentical(ca1,result);
+            // find by invalid id
+            result=pm.findAttributeById(-1);
+            this.areIdentical(result, null);
+        }
+
+        findAttributeByQualifier(){
+            var pm = new cpm.dolphin.ClientPresentationModel(undefined,undefined);
+            var ca1 = new ca.dolphin.ClientAttribute("prop1","qual1","value1","VALUE");
+
+            pm.addAttribute(ca1);
+            var result = pm.findAttributeByQualifier("qual1");
+            this.areIdentical(ca1,result);
+            // find by invalid qualifier
+            result=pm.findAttributeByQualifier("dummy");
+            this.areIdentical(result, null);
+        }
+
+        findAttributeByPropertyNameAndTag(){
+            var pm = new cpm.dolphin.ClientPresentationModel(undefined,undefined);
+            var ca1 = new ca.dolphin.ClientAttribute("prop1","qual1","value1","VALUE");
+
+            pm.addAttribute(ca1);
+            var result = pm.findAttributeByPropertyNameAndTag("prop1","VALUE");
+            this.areIdentical(ca1,result);
+            // find by invalid property name
+            result=pm.findAttributeByPropertyNameAndTag("dummy","VALUE");
+            this.areIdentical(result, null);
+        }
+
+        findAllAttributesByPropertyName(){
+            var pm = new cpm.dolphin.ClientPresentationModel(undefined,undefined);
+            var ca1 = new ca.dolphin.ClientAttribute("prop1","qual1","value1","VALUE");
+            var ca2 = new ca.dolphin.ClientAttribute("prop1","qual2","value2","TOOLTIP");
+
+            pm.addAttribute(ca1);
+            pm.addAttribute(ca2);
+            var result = pm.findAllAttributesByPropertyName("prop1");
+            this.areIdentical(result.length, 2);
+            this.areIdentical(ca1,result[0]);
+            this.areIdentical(ca2,result[1]);
+            // find by invalid property name
+            result=pm.findAllAttributesByPropertyName("dummy");
+            this.areIdentical(result.length, 0);
+        }
     }
 }
