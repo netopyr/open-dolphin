e77c85fae19ab0ce755264ab2de65b364fbaa697
Christoph Lipp <christoph.lipp@canoo.com>
2014-03-07 18:15:30 +0100

    DOL-107 binding: make new "using" of converters in JFXBinder the same as in Binder

--- subprojects/client-javafx/src/main/groovy/org/opendolphin/binding/BindClientOtherOfAble.groovy
+++ subprojects/client-javafx/src/main/groovy/org/opendolphin/binding/BindClientOtherOfAble.groovy
@@ -5,18 +5,30 @@ import org.opendolphin.core.client.ClientAttribute
 class BindClientOtherOfAble {
     final ClientAttribute attribute
     final String targetPropertyName
+    final Converter converter
 
-    BindClientOtherOfAble(ClientAttribute attribute, String targetPropertyName) {
+    BindClientOtherOfAble(ClientAttribute attribute, String targetPropertyName, Converter converter) {
         this.attribute = attribute
         this.targetPropertyName = targetPropertyName
+        this.converter = converter
     }
 
-    void of(Object target, Closure converter = null) {
-        of target, converter == null ? null : new ConverterAdapter(converter)
+    @Deprecated // TODO (DOL-93) remove legacy code
+    void of(Object target, Closure converter) {
+        of target, new ConverterAdapter(converter)
     }
+
+    @Deprecated // TODO (DOL-93) remove legacy code
     void of(Object target, Converter converter) {
         def listener = new JFXBinderPropertyChangeListener(attribute, target, targetPropertyName, converter)
         attribute.addPropertyChangeListener('value', listener)
         listener.update() // set the initial value after the binding and trigger the first notification
     }
+
+    void of(Object target) {
+        def listener = new JFXBinderPropertyChangeListener(attribute, target, targetPropertyName, converter)
+        attribute.addPropertyChangeListener('value', listener)
+        listener.update() // set the initial value after the binding and trigger the first notification
+    }
+
 }
