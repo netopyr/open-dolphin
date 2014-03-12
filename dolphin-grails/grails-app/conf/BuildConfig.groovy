19d3d6af2c42829a31fa4a0c9ce29c171bbef7f0
Dierk Koenig <dierk.koenig@canoo.com>
2014-03-13 00:34:23 +0100

    make version 0.9

--- dolphin-grails/grails-app/conf/BuildConfig.groovy
+++ dolphin-grails/grails-app/conf/BuildConfig.groovy
@@ -48,7 +48,7 @@ grails.project.dependency.resolution = {
     dependencies {
         compile 'org.codehaus.gpars:gpars:1.0.0'
 
-        def dolphinVersion = '1.0-SNAPSHOT'
+        def dolphinVersion = '0.9'
 
         compile "org.open-dolphin:dolphin-shared:$dolphinVersion"
         compile "org.open-dolphin:dolphin-server:$dolphinVersion"
