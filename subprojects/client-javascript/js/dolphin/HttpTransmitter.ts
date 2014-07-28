f855492ec0405d693d48e03c4f2cc97f04866081
Kunal Singh <kunal.singh@canoo.com>
2014-07-28 14:48:04 +0530

    DOL-125 : HttpStatus codes are encapsulated in a constant.

--- subprojects/client-javascript/js/dolphin/HttpTransmitter.ts
+++ subprojects/client-javascript/js/dolphin/HttpTransmitter.ts
@@ -10,6 +10,10 @@ export module dolphin {
         http:XMLHttpRequest;
         codec:cod.dolphin.Codec
 
+        HttpCodes = {
+            finished: 4,
+            success : 200
+        }
         constructor(public url: string, reset: boolean = true) {
             this.http = new XMLHttpRequest();
 //            this.http.withCredentials = true; // not supported in all browsers
@@ -27,11 +31,15 @@ export module dolphin {
             }
 
             this.http.onreadystatechange= (evt:ProgressEvent) => {
-                if (this.http.readyState==4 && this.http.status==200)
-                {
-                    var responseText = this.http.responseText;
-                    var responseCommands = this.codec.decode(responseText);
-                    onDone(responseCommands);
+                if (this.http.readyState == this.HttpCodes.finished){
+
+                    if(this.http.status == this.HttpCodes.success)
+                    {
+                        var responseText = this.http.responseText;
+                        var responseCommands = this.codec.decode(responseText);
+                        onDone(responseCommands);
+                    }
+                    //todo ks: if status is not 200 then show error
                 }
             }
 
