4c39f8703b835384b88cc8a85593df69e6fbd31a
Dierk Koenig <dierk.koenig@canoo.com>
2014-11-10 00:05:30 +0100

    starting the 0.12 release train

--- dolphin-grails/grails-app/conf/BuildConfig.groovy
+++ dolphin-grails/grails-app/conf/BuildConfig.groovy
@@ -48,7 +48,7 @@ grails.project.dependency.resolution = {
     dependencies {
         compile 'org.codehaus.gpars:gpars:1.0.0'
 
-        def dolphinVersion = '0.11'
+        def dolphinVersion = '0.12-SNAPSHOT'
 
         compile "org.open-dolphin:dolphin-shared:$dolphinVersion"
         compile "org.open-dolphin:dolphin-server:$dolphinVersion"
