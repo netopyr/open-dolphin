7aa052fc348466dc0221cc096ec0afb37edc42aa
Dierk Koenig <dierk.koenig@canoo.com>
2014-11-09 21:59:52 +0100

    update to release 0.11

--- dolphin-grails/grails-app/conf/BuildConfig.groovy
+++ dolphin-grails/grails-app/conf/BuildConfig.groovy
@@ -48,7 +48,7 @@ grails.project.dependency.resolution = {
     dependencies {
         compile 'org.codehaus.gpars:gpars:1.0.0'
 
-        def dolphinVersion = '0.11-SNAPSHOT'
+        def dolphinVersion = '0.11'
 
         compile "org.open-dolphin:dolphin-shared:$dolphinVersion"
         compile "org.open-dolphin:dolphin-server:$dolphinVersion"
