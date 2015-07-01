b358743305faa52ea2a35dfad72df2b11ca794aa
Dave Kriewall <dave.kriewall@gmail.com>
2015-07-01 14:44:12 -0700

    Refine documentation.  Fix old 'bind' syntax in MasterDetailView.

--- subprojects/combined/src/main/groovy/org/opendolphin/core/comm/DefaultInMemoryConfig.groovy
+++ subprojects/combined/src/main/groovy/org/opendolphin/core/comm/DefaultInMemoryConfig.groovy
@@ -23,6 +23,12 @@ import org.opendolphin.core.client.comm.InMemoryClientConnector
 import org.opendolphin.core.server.DefaultServerDolphin
 import org.opendolphin.core.server.ServerDolphinFactory
 
+/**
+ * Base class for running a client and server dolphin inside the same VM.
+ * <p/>
+ * Subclasses JavaFxInMemoryConfig and SwingInMemoryConfig additionally set the threading model
+ * as appropriate for the UI (JavaFX or Swing, respectively.)
+ */
 class DefaultInMemoryConfig {
 
     ClientDolphin clientDolphin = new ClientDolphin()
