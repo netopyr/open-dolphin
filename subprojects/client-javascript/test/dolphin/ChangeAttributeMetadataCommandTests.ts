a6b2e8b1b60cd141a53a28f8b55655b6a69c360a
gema <gema.santiago@canoo.com>
2015-09-04 15:50:57 +0200

    testing teamcity build

--- subprojects/client-javascript/test/dolphin/ChangeAttributeMetadataCommandTests.ts
+++ subprojects/client-javascript/test/dolphin/ChangeAttributeMetadataCommandTests.ts
@@ -1,4 +1,3 @@
-
 /// <reference path="../../testsuite/tsUnit.ts"/>
 /// <reference path="../../js/dolphin/ChangeAttributeMetadataCommand.ts"/>
 
@@ -12,7 +11,7 @@ module opendolphin {
             this.areIdentical(changedAttrMDCommand.className,"org.opendolphin.core.comm.ChangeAttributeMetadataCommand");
             this.areIdentical(changedAttrMDCommand.attributeId, "10");
             this.areIdentical(changedAttrMDCommand.metadataName,"MDName");
-            this.areIdentical(changedAttrMDCommand.value,25);
+            this.areIdentical(changedAttrMDCommand.value,20);
 
         }
 
