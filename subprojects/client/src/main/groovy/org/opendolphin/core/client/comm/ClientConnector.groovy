612af7bc80456a2263d7243c503978888c513d26
Jochen Theodorou <blackdrag@gmx.org>
2015-12-15 16:11:56 +0100

    change clientModelStore to use type ClientModelStore instead of Object

--- subprojects/client/src/main/groovy/org/opendolphin/core/client/comm/ClientConnector.groovy
+++ subprojects/client/src/main/groovy/org/opendolphin/core/client/comm/ClientConnector.groovy
@@ -92,7 +92,7 @@ abstract class ClientConnector {
         flow.start(1)
     }
 
-    protected getClientModelStore() {
+    protected ClientModelStore getClientModelStore() {
         clientDolphin.clientModelStore
     }
 
@@ -187,7 +187,7 @@ abstract class ClientConnector {
 
     @CompileStatic
     ClientPresentationModel handle(CreatePresentationModelCommand serverCommand) {
-        if (((ClientModelStore) clientModelStore).containsPresentationModel(serverCommand.pmId)) {
+        if (clientModelStore.containsPresentationModel(serverCommand.pmId)) {
             throw new IllegalStateException("There already is a presentation model with id '$serverCommand.pmId' known to the client.")
         }
         List<ClientAttribute> attributes = []
@@ -208,7 +208,7 @@ abstract class ClientConnector {
         if (serverCommand.clientSideOnly) {
             model.clientSideOnly = true
         }
-        ((ClientModelStore)clientModelStore).add(model)
+        clientModelStore.add(model)
         clientDolphin.updateQualifiers(model)
         return model
     }
