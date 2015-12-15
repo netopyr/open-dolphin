77c2449b8f69a102a7a6d8de31adeda2a0608e0c
Jochen Theodorou <blackdrag@gmx.org>
2015-12-15 17:19:00 +0100

    make some classes final

--- subprojects/shared/src/main/groovy/org/opendolphin/core/Tag.groovy
+++ subprojects/shared/src/main/groovy/org/opendolphin/core/Tag.groovy
@@ -20,7 +20,7 @@ package org.opendolphin.core
  * Several tags are predeclared; see documentation for suggested usage.
  * @see Attribute
  */
-class Tag {
+final class Tag {
 
     final String name;
 
