7d788073c179416e2ebabc974a060d4f5720f80f
Dierk Koenig <dierk.koenig@canoo.com>
2016-02-26 10:34:37 +0100

    add todo for later inspection

--- subprojects/demo-javafx/client/src/main/groovy/org/opendolphin/demo/team/TeamStarter.java
+++ subprojects/demo-javafx/client/src/main/groovy/org/opendolphin/demo/team/TeamStarter.java
@@ -10,15 +10,17 @@ import org.opendolphin.core.client.comm.JavaFXUiThreadHandler;
 import org.opendolphin.core.comm.JsonCodec;
 
 import java.io.File;
+import java.lang.reflect.Field;
 import java.net.URI;
+import java.util.HashMap;
 
 // todo: make url base configurable from command line
 
 public class TeamStarter {
     public static void main(String[] args) throws Exception {
 
-        final String servletUrl = "http://localhost:8080/dolphin-grails/dolphin/";
-//        final String servletUrl = "https://klondike.canoo.com/dolphin-grails/dolphin/";
+//        final String servletUrl = "http://localhost:8080/dolphin-grails/dolphin/";
+        final String servletUrl = "https://klondike.canoo.com/dolphin-grails/dolphin/";
 
         ClientDolphin clientDolphin = new ClientDolphin();
         clientDolphin.setClientModelStore(new ClientModelStore(clientDolphin));
@@ -34,9 +36,21 @@ public class TeamStarter {
 
         TeamApplication.clientDolphin = clientDolphin;
 
-        String result = connector.uploadFile(new File("/data.txt"), DefaultGroovyMethods.toURI("http://localhost:8080/dolphin-grails/fileUpload/save"));
-        System.out.println("uploaded and returned: "+result);
+//        String result = connector.uploadFile(new File("/data.txt"), DefaultGroovyMethods.toURI("http://localhost:8080/dolphin-grails/fileUpload/save"));
+//        System.out.println("uploaded and returned: "+result);
+
+        // Mac-specific hack for java 7 on el capitan
+        try {
+            Class<?> macFontFinderClass = Class.forName("com.sun.t2k.MacFontFinder");
+            Field psNameToPathMap = macFontFinderClass.getDeclaredField("psNameToPathMap");
+            psNameToPathMap.setAccessible(true);
+            psNameToPathMap.set(null, new HashMap<String, String>());
+        } catch (Exception e) {
+            // ignore
+        }
+
 
         Application.launch(TeamApplication.class);
+
     }
 }
